ee4bbdbee5e39603ab5fbc6a6cea25de553a44a9
Milena <milenarosa18@gmail.com>
2021-11-05 18:36:00 -0300

    validando para não aceitar números negativos

--- ex011.py
+++ ex011.py
@@ -1,6 +1,9 @@
 larg = float(input('Largura da parede: '))
 alt = float(input('Altura da parede: '))
-área = larg * alt
-print('Sua parede tea a dimensão de {}x{} e sua área é de {}m².'.format(larg, alt, área))
-tinta = área / 2
-print('Para pintar essa parede, você precisará de {}l de tinta.'.format(tinta))
+if (alt<0 or larg<0):
+    print('Altura ou largura inválida')
+else:
+    área = larg * alt
+    print('Sua parede tea a dimensão de {}x{} e sua área é de {}m².'.format(larg, alt, área))
+    tinta = área / 2
+    print('Para pintar essa parede, você precisará de {}l de tinta.'.format(tinta))
